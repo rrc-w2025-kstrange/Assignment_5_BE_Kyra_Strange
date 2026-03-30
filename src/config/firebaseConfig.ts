666026e4f1100e4574c08fb4800cf900f5f1fca9
Kyra Strange <kstrangeguajardo@academic.rrc.ca>
2026-03-29 20:52:17 -0500

    feat: reverted this change as it breakes firebase config atm- Configure firebaseConfig to Use Environment Variables

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -1,21 +1,21 @@
 import { initializeApp, cert, ServiceAccount } from "firebase-admin/app";
 import { getFirestore, Firestore } from "firebase-admin/firestore";
 
-// import * as serviceAccount from "./servicekey.json";
+import * as serviceAccount from "./servicekey.json";
 
-const {
-        FIREBASE_PROJECT_ID,
-        FIREBASE_CLIENT_EMAIL,
-        FIREBASE_PRIVATE_KEY,
-    } = process.env;
+// const {
+//         FIREBASE_PROJECT_ID,
+//         FIREBASE_CLIENT_EMAIL,
+//         FIREBASE_PRIVATE_KEY,
+//     } = process.env;
 
-     // Create a service account object with the provided credentials
-    const serviceAccount: ServiceAccount = {
-        projectId: FIREBASE_PROJECT_ID,
-        clientEmail: FIREBASE_CLIENT_EMAIL,
-        // Replace escaped newlines in the private key string with actual newlines
-        privateKey: FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
-    };
+//      // Create a service account object with the provided credentials
+//     const serviceAccount: ServiceAccount = {
+//         projectId: FIREBASE_PROJECT_ID,
+//         clientEmail: FIREBASE_CLIENT_EMAIL,
+//         // Replace escaped newlines in the private key string with actual newlines
+//         privateKey: FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
+//     };
 
 // Initialize the Firebase app with the service account credentials
 initializeApp({
