761abfb784d66706046487cba935629d1341fab0
Kyra Strange <kstrangeguajardo@academic.rrc.ca>
2026-03-29 20:59:28 -0500

    feat: Updated Helmet Implementation in app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,13 @@ import dotenv from "dotenv";
 dotenv.config();
 import eventRoutes from "./api/v1/routes/eventRoutes";
 import setupSwagger from "./config/swagger";
-import helmet from "helmet";
+import { getHelmetConfig } from "./config/helmetConfig";
 
 
 // Initialize Express application
 const app: Express = express();
 
-app.use(helmet());
+app.use(getHelmetConfig());
 
 app.use(express.json());
 
